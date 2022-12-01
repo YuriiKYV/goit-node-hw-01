6a15e8ed22c72b828989a774664374be44bde00b
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-12-01 22:57:49 +0200

    save

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath)
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const resault = contacts.find(item => item.id === `${contactId}`);
+
+  return resault || null;
+}
+
+async function removeContact(contactId) {
+  // ...твой код
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  }
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
